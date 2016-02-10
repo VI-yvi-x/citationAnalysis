30f16b170bc657789fe13c58e40e7e1d79b0a7fa
Ilya Vasilyev <ilya.vasilev@phystech.edu>
2016-02-10 17:44:56 +0300

    Some additions

--- experiments.R
+++ experiments.R
@@ -2,7 +2,18 @@
 Citations = read.csv("citations.csv")
 Merits = read.csv("merits.csv")
 CombineStatistics = read.csv("combines.csv")
+ExampleSet = read.csv("example.csv")
+Taxonomy = read.csv("taxonomy.csv")
 
 #Investigate data.
 summary(Citations)
 str(Citations)
+
+#Visuals
+install.packages("ggplot")
+library(ggplot)
+
+str(Taxonomy)
+
+plot(ExampleSet$Housing, ExampleSet$Food)
+hist(Taxonomy$Name)
